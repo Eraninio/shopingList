49328e1e6ed7a67c11fcdf7eae762ad7178396c1
Eraninio <67857483+Eraninio@users.noreply.github.com>
2020-08-06 01:19:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,47 @@ const app = express();
 app.use(express.json());
 
 const products = [
-    {id: "1", productName: "milk"},
-    {id: "2", productName: "bread"},
-    {id: "3", productName:"cheese"},
-    {id: "4", productName: "water"},
-    {id: "5", productName: "oil"}
+    {id: "1", productName: "Milk"},
+    {id: "2", productName: "Bread"},
+    {id: "3", productName:"Cheese"},
+    {id: "4", productName: "Water"},
+    {id: "5", productName: "Oil"}
 ];
 
 app.get("/products", (req, res) => {
-    res.send(myList);
+    res.send(products);
 });
 
+app.get("/products/:Id", (req, res) =>
+    products.forEach((item => {
+        if (item.id === req.params.Id){
+            res.send(item.productName);
+        }
+    })
+));
 
+app.post("/products", (req, res) => {
+    products.push(req.body);
+    res.send(req.body);
+});
 
+app.put("/products/:Id", (req, res) =>{
+    products.forEach((item) => {
+        if (item.id === req.params.Id) {
+            products.splice(products.indexOf(item), 1, req.body)
+            res.send(products);
+        }
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-
+app.delete("/products/:Id", (req, res) =>{
+    products.forEach((item) => {
+        if (item.id === req.params.Id) {
+            products.splice(products.indexOf(item), 1)
+            res.send(products);
+        }
+    });
+});
 
 
 app.listen(3000);
